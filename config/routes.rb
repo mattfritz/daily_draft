58bc32a0284fd43a643499b072ff08dadc3d7e59
Matt Fritz <mfritzer@gmail.com>
2013-09-22 16:44:43 -0600

    fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 DailyDraft::Application.routes.draw do
   resources :entries
-
+  
+  root :to => "entries#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
